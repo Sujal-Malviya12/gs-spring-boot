d954ed439983519eebe5fbe7b9246b969afd837d
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2026-02-03 15:45:40 +0530

    Simulate slower API response for perf test

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+  
   @GetMapping("/")
-  public String index() {
-    return "Greetings from Spring Boot!";
+public String index() {
+  try {
+    Thread.sleep(200); // simulate slow processing (200ms)
+  } catch (InterruptedException e) {
+    Thread.currentThread().interrupt();
   }
+  return "Greetings from Spring Boot!";
+}
+
 
 }
